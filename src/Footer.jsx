22ef43d0bf32002d36d657448f01625c3417e15d
pitamchandra <pitamchandra402@gmail.com>
2023-05-26 15:58:55 +0600

    update footer design

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p className='text-center py-5 divide-y text-gray-600'>&copy; Copyright by Pitam Chandra</p>
+            <p className='text-center py-5 divide-y text-gray-600'>&copy; Copyright by <span className="text-orange-500">Pitam Chandra</span></p>
         </div>
     );
 };
